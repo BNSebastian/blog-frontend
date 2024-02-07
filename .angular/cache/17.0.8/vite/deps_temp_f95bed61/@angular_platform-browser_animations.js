c7fd09370571e9aff9d361ca1733554e221cfde0
BNSebastian <bucovala.sebastian@protonmail.com>
2024-02-07 23:17:54 +0200

    UPDATE

---
+++ .angular/cache/17.0.8/vite/deps_temp_f95bed61/@angular_platform-browser_animations.js
@@ -0,0 +1,26 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-QMNYVUH4.js";
+import "./chunk-MZZ5QAHR.js";
+import "./chunk-EHKLKERD.js";
+import "./chunk-7HIBBP5U.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-UYHU5JHP.js";
+import "./chunk-BQTYKBYB.js";
+import "./chunk-WI6LBH4V.js";
+import "./chunk-KDOJNZN6.js";
+import "./chunk-OXCW2X5T.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
