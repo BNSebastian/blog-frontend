a3799f294fcfef6d4599115df3e91015937841cb
BNSebastian <bucovala.sebastian@protonmail.com>
2024-02-11 00:30:41 +0200

    update

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,66 +1,67 @@
-import { map, Observable } from 'rxjs';
+import { map, Observable } from "rxjs";
 
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
 
-import { BACKEND, backendUrl } from '../../shared/environments/backend';
-import { AuthenticationRequest, RegisterRequest, User } from '../models/user';
-import { CustomCookieService } from './custom-cookie.service';
+import { BACKEND, backendUrl } from "../../shared/environments/backend";
+import { AuthenticationRequest, RegisterRequest, User } from "../models/user";
+import { CustomCookieService } from "./custom-cookie.service";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class AuthService {
-  private cookieService = inject(CustomCookieService);
-  private http = inject(HttpClient);
+    private cookieService = inject(CustomCookieService);
+    private http = inject(HttpClient);
 
-  register(model: RegisterRequest): Observable<User> {
-    return this.http.post<User>(backendUrl.register, model).pipe(
-      map((response: User) => {
-        this.cookieService.saveUserCookies(response);
-        return response;
-      })
-    );
-  }
+    register(model: RegisterRequest): Observable<User> {
+        return this.http.post<User>(backendUrl.register, model).pipe(
+            map((response: User) => {
+                this.cookieService.saveUserCookies(response);
+                return response;
+            })
+        );
+    }
 
-  authenticate(model: AuthenticationRequest): Observable<User> {
-    return this.http.post<User>(backendUrl.authenticate, model).pipe(
-      map((response: User) => {
-        this.cookieService.saveUserCookies(response);
-        return response;
-      })
-    );
-  }
+    authenticate(model: AuthenticationRequest): Observable<User> {
+        console.log(backendUrl.authenticate);
+        return this.http.post<User>(backendUrl.authenticate, model).pipe(
+            map((response: User) => {
+                this.cookieService.saveUserCookies(response);
+                return response;
+            })
+        );
+    }
 
-  isAuthenticated() {
-    return this.cookieService.checkToken();
-  }
+    isAuthenticated() {
+        return this.cookieService.checkToken();
+    }
 
-  isAdmin(): Observable<boolean> {
-    const userId = {
-      userId: Number(this.cookieService.getUserId()),
-    };
+    isAdmin(): Observable<boolean> {
+        const userId = {
+            userId: Number(this.cookieService.getUserId()),
+        };
 
-    return this.http.post<boolean>(backendUrl.isAdmin, userId).pipe(
-      map((response: boolean) => {
-        return response;
-      })
-    );
-  }
+        return this.http.post<boolean>(backendUrl.isAdmin, userId).pipe(
+            map((response: boolean) => {
+                return response;
+            })
+        );
+    }
 
-  logout() {
-    this.cookieService.destroyUserCookies();
-  }
+    logout() {
+        this.cookieService.destroyUserCookies();
+    }
 
-  getUserDetails(): User {
-    const user: User = {
-      id: this.cookieService.getUserId(),
-      firstname: this.cookieService.getUserFirstname(),
-      lastname: this.cookieService.getUserLastname(),
-      email: this.cookieService.getUserEmail(),
-      token: this.cookieService.getToken(),
-    };
+    getUserDetails(): User {
+        const user: User = {
+            id: this.cookieService.getUserId(),
+            firstname: this.cookieService.getUserFirstname(),
+            lastname: this.cookieService.getUserLastname(),
+            email: this.cookieService.getUserEmail(),
+            token: this.cookieService.getToken(),
+        };
 
-    return user;
-  }
+        return user;
+    }
 }
--- src/app/shared/environments/backend.ts
+++ src/app/shared/environments/backend.ts
@@ -1,6 +1,5 @@
-const backendHost = "bucovala-sebastian.go.ro"; // Docker service name
-// const backendHost = "backend-service"; // Docker service n
-const backendPort = "8080"; // Replace this with your backend port
+const backendHost = "bucovala-sebastian.go.ro";
+const backendPort = "8080";
 const baseUrl: string = `http://${backendHost}:${backendPort}/api`;
 
 export const backendUrl = {
